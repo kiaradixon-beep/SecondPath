abfff9ef4dd536c522ee7eb568e82962b2173304
kiaradixon-beep <kiaradixon91@gmail.com>
2026-04-07 08:15:30 -0400

    Create server.js

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.use('/api', require('./routes/analytics'));
+app.use('/api', require('./routes/auth'));
+app.use('/api', require('./routes/behaviors'));
+app.use('/api', require('./routes/facilities'));
+app.use('/api', require('./routes/reports'));
+
+app.get('/', (req, res) => {
+    res.send('Youth Behavior Tracking System API is running');
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
